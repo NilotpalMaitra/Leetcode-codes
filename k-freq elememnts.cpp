4eadd386aa865e55025f8336fcc51f9953e02625
Nilot <128976944+NilotpalMaitra@users.noreply.github.com>
2024-01-30 23:49:52 +0530

    Update k-freq elememnts.cpp

--- k-freq elememnts.cpp
+++ k-freq elememnts.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        // Create a hash map to store the frequency of each element
+        unordered_map<int, int> counts;
+        
+        // Create a max heap to store elements with their frequencies
+        priority_queue<pair<int, int>> maxi;
        
-        unordered_map <int,int>counts; 
-        priority_queue<pair<int,int>>maxi;
-       
-        for(int i=0;i<nums.size();i++){
+        // Count the occurrences of each element in nums
+        for(int i = 0; i < nums.size(); i++) {
             counts[nums[i]]++;
         }
 
-       
-        while(!counts.empty()){
-            maxi.push({counts.begin()->second,counts.begin()->first});
+        // Transfer elements and their frequencies from the hash map to the max heap
+        while (!counts.empty()) {
+            maxi.push({counts.begin()->second, counts.begin()->first});
             counts.erase(counts.begin());
         }
 
-        vector<int>ans;
-        for(int i=0;i<k;i++){
+        // Retrieve the top k elements from the max heap
+        vector<int> ans;
+        for (int i = 0; i < k; i++) {
             ans.push_back(maxi.top().second);
             maxi.pop();
         }
+
         return ans;
     }
 };
